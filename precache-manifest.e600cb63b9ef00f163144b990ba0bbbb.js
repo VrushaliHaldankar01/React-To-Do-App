f697ffa29b44dfbeb7768efe6e18b7287f1cbaa8
Vrushali <vrushalihaldankar01@gmail.com>
2022-10-31 17:01:56 +0530

    Updates

---
+++ precache-manifest.e600cb63b9ef00f163144b990ba0bbbb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f40e41c796614c3d5ce1a9393363211",
+    "url": "/React-To-Do-App/index.html"
+  },
+  {
+    "revision": "a5fb524a0815888f27c8",
+    "url": "/React-To-Do-App/static/js/2.eb38b736.chunk.js"
+  },
+  {
+    "revision": "43bc7d2fa108a9142e82",
+    "url": "/React-To-Do-App/static/js/main.4eddced1.chunk.js"
+  },
+  {
+    "revision": "bc1798a517617bbbe9d1",
+    "url": "/React-To-Do-App/static/js/runtime-main.90e73874.js"
+  }
+]);
